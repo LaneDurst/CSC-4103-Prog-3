b7606a96ea84511ab4ea1863d0cb3d878e7712bf
LaneDurst <143672256+LaneDurst@users.noreply.github.com>
2024-11-21 15:25:27 -0600

    moved some stuff around to make file nav easier

--- filesystem.c
+++ filesystem.c
@@ -1,1 +1,15 @@
-// THIS IS THE ONLY FILE WE ARE EDITING //
+// THIS AND FORMATFS.C ARE THE ONLY FILES WE ARE EDITING [Lane] //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <pthread.h>
+
+File open_file(char *name, FileMode mode)
+{
+
+}
