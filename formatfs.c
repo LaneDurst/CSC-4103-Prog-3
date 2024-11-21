0519a34330b53747bad29085aba919259b397823
LaneDurst <ltdurst03@gmail.com>
2024-11-21 16:10:35 -0600

    added comments

--- formatfs.c
+++ formatfs.c
@@ -1,1 +1,3 @@
 // THIS and FILESYSTEM.C ARE THE ONLY FILES WE ARE EDITING [Lane] //
+// The purpose of this file is to mess with system initialization //
+// look at softwaredisk.h or softwaredisk.c //
