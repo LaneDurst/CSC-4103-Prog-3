466a9cfb2d6b6dd90562e24d9f61a39a92e5b4a1
LaneDurst <ltdurst03@gmail.com>
2024-11-23 15:25:01 -0600

    added given methods

--- formatfs.c
+++ formatfs.c
@@ -1,3 +1,31 @@
 // THIS and FILESYSTEM.C ARE THE ONLY FILES WE ARE EDITING [Lane] //
 // The purpose of this file is to mess with system initialization //
-// look at softwaredisk.h or softwaredisk.c //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <pthread.h>
+#include "softwaredisk.h"
+
+//might remove this include
+#include "filesystem.h"
+
+// these bitmap methods were all provided by golden
+void set_bit(unsigned char* bitmap, uint64_t j)
+{
+    bitmap[j/8] != (1 << (j%8));
+}
+
+void clear_bit(unsigned char* bitmap, uint64_t j)
+{
+    bitmap[j/8] &= ~(1 << (j%8));
+}
+
+bool is_bit_set(unsigned char *bitmap, uint64_t j)
+{
+    return bitmap[j/8] & (1 << (j%8));
+}
